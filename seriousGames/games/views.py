48541d25573acce4a80b5c5733e7bd8dff1aba4d
Jose Antonio <joseantonio.sanchezf26@gmail.com>
2025-06-02 15:11:07 +0200

    Incluido WildAI

--- seriousGames/games/views.py
+++ seriousGames/games/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 
 @login_required
 def game_selection(request):
-    games = ["SurvivalRules", "Machine Learning Game"]  # Puedes obtener esto de la base de datos
+    games = ["SurvivalRules", "WildAI"]  # Puedes obtener esto de la base de datos
     return render(request, 'game_selection.html', {'games': games})
 
 def play_game(request, game_name):
