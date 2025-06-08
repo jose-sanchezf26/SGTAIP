ad969367cb2ace1ce46bceea014bb73f06110366
Jose Antonio <joseantonio.sanchezf26@gmail.com>
2025-06-08 18:11:56 +0200

    Página de descarga de WildAI añadida

--- seriousGames/games/urls.py
+++ seriousGames/games/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('wildai/', views.wildai_instructions, name='wildai_instructions'),
     path('game_selection', views.game_selection, name='game_selection'),  
     path('<str:game_name>/', views.play_game, name='play_game'),  
 ]
--- seriousGames/games/views.py
+++ seriousGames/games/views.py
@@ -6,8 +6,11 @@ from django.shortcuts import redirect
 
 @login_required
 def game_selection(request):
-    games = ["SurvivalRules", "WildAI"]  # Puedes obtener esto de la base de datos
+    games = ["SurvivalRules"]  # Puedes obtener esto de la base de datos
     return render(request, 'game_selection.html', {'games': games})
 
 def play_game(request, game_name):
      return render(request, "game_template.html", {"game_name": game_name, "username": request.user.username})
+
+def wildai_instructions(request):
+    return render(request, 'wildAI.html')
